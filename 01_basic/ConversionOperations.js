8675d461481927b3fdd8fcc22628eaa277f739e4
Roshan Kewat <kewatroshan83@gmail.com>
2026-01-06 14:23:39 +0000

    ConversionOfDatatype

---
+++ 01_basic/ConversionOperations.js
@@ -0,0 +1,24 @@
+//let score="Roshan"
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+//let valueinnumber=Number(score)
+//console.log(typeof valueinnumber)
+//console.log(valueinnumber)
+
+//"33" => 33
+//"33abc" =>NaN   Not an Number (type=number)
+//true =1 false=0
+
+let isloggedIn=""
+
+let booleanisloogedIN= Boolean(isloggedIn);
+console.log(booleanisloogedIN);
+
+//"Roshan"=> True
+//""=> False
+
+let somenumber='abc'
+let stringnumber=Number(somenumber)
+console.log(typeof stringnumber);
--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,1 +1,5 @@
 console.log("roshan")
+const accountnumber=12345
+console.log(accountnumber)
+let password="12345"
+console.table([accountnumber,password])
